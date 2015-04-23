7a702d52726353765694117a74bc8628e0c39712
Magnus Bergman <karlmagnusbergman@gmail.com>
2015-04-23 08:00:13 +0200

    Update comments for revision task handler.

--- gulp/tasks/rev/index.js
+++ gulp/tasks/rev/index.js
@@ -4,6 +4,8 @@ var gulp = require('gulp');
 // 1) Add hashes to assets referenced by CSS and JS files
 // 2) Update asset references with reved filenames in compiled css + js
 // 3) Update asset references in HTML
+// 4) Rev and compress CSS and JS files (this is done after assets, so that if
+//    a referenced asset hash changes, the parent hash will change as well.
 // 4) Report filesizes
 
 gulp.task('rev', [
